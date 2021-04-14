b7e9a95779cf671ee28eec87e3b60e911324212e
Rachid Rakaa <rachid1982fsb@gmail.com>
2021-04-13 21:28:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,34 +14,7 @@ const todaydate= document.getElementById("date")
 const users = {
                AbdElssamia_Rakaa: [7,8],
                Yahya_Rakaa: [15,16],
-               HajMuhamed_Rakaa: [3,4],
-               //Imran_Rakaa: [7,8],
-               //IbnAbdelmoumen_Rakaa: [9,10],
-               //HajHassan_Rakaa: [11,12],
-               //Simohammed_Rakaa: [13,14],
-               Said_Elkriti: [29,30],
-               Abdelmoumen_Rakaa: [19,20],
-               Mastapha_Mouarif: [23,24],
-               Omar_Lakriti: [33,34],
-               Abdelwadoud_Rakaa: [5,6],
-               //Miloud_Mouarif: [25,26],
-               //Hamza_Mouarif: [27,28],
-               Abdelssamad_Rakaa: [11,12],
-               Adil_Rakaa: [9,10],
-               Rachid_Rakaa: [13,14],
-               Abdelwahab_Rakaa: [21,22],
-               Amine_Mouarif: [27,28],
-               Ayoub_Mouarif: [25,26],
-               //Mohammed_Atari: [41,42],
-               Sadiq_Rakaa: [17,18],
-               //Mohamed_Lakriti: [45,46],
-               Khalid_Rakaa: [1,2],
-               //Ahmed_Rakaa: [49,50],
-               //AbdEllah_Rakaa: [51,52],
-              // Aziz_Rakaa: [53,54],
-               Aziz_Lakriti: [31,32],
-               //Abdelhaq_Qadach: [57,58],
-               Abderrahman_Rakaa: [35,36]
+               HajMuhamed_Rakaa: [3,4]
             }
             
 
