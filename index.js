bee3bfca07e77f7a0f59acfac1c27721b0f9044e
Rachid Rakaa <rachid1982fsb@gmail.com>
2021-04-13 21:17:02 -0400

    update the names and the start date to 14

--- index.js
+++ index.js
@@ -89,8 +89,8 @@ function dateInRamadan(tDate, rStart){
 // a function that take inital ahzab for the first day of ramadan and return the current ahzab based of today date
 const getTodayAhzab=(arr)=>{
 
-    const start = (arr[0] + 2*(dateInRamadan(dateN, 13) -1 )) <= 60 ? (arr[0] + 2*(dateInRamadan(dateN, 13) -1 )) : (arr[0] + 2*(dateInRamadan(dateN, 13) -1 ))-60
-    const end = (arr[1] + 2*(dateInRamadan(dateN, 13) -1 )) <= 60 ? (arr[1] + 2*(dateInRamadan(dateN, 13) -1 )) : (arr[1] + 2*(dateInRamadan(dateN, 13) -1 ))-60
+    const start = (arr[0] + 2*(dateInRamadan(dateN, 14) -1 )) <= 60 ? (arr[0] + 2*(dateInRamadan(dateN, 14) -1 )) : (arr[0] + 2*(dateInRamadan(dateN, 14) -1 ))-60
+    const end = (arr[1] + 2*(dateInRamadan(dateN, 14) -1 )) <= 60 ? (arr[1] + 2*(dateInRamadan(dateN, 14) -1 )) : (arr[1] + 2*(dateInRamadan(dateN, 14) -1 ))-60
     
     formN.innerText=start
     toN.innerText=end
